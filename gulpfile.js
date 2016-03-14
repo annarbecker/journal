7bcb947a025a39607e8ffb18b172a73cfb75a94d
Anna Becker and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-14 11:09:51 -0700

    clean before build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp');
+var del = require('del');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var buildProduction = utilities.env.production;
+
 
 gulp.task('jsBrowserify', function() {
   return browserify({ entries: ['./js/browser.js'] })
@@ -14,4 +18,24 @@ gulp.task("minifyScripts", ["jsBrowserify"], function() {
   return gulp.src("./build/js/app.js")
   .pipe(uglify())
   .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("build", function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+
+gulp.task("clean", function(){
+  return del(['build']);
+});
+
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
